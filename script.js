611ae186cc83c3fc55fcd001b23f7722efd73e03
Toni Panzera <toni@panzera.co.za>
2026-04-30 08:51:09 -0500

    updates

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const tabs = document.querySelectorAll("[data-tab-target]")
-const tabContents = document.querySelectorAll("[data-tab-content]")
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 
 tabs.forEach( tab => {
-    tab.addEventListener("click", () => {
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
